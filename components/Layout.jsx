46651850313c8a6109c9ab1710bb8252abf6481d
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-08 11:14:02 +0200

    Add footer component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,25 @@
 import Head from 'next/head';
 import React from 'react';
+import Footer from './Footer';
 import NavBar from './NavBar';
 
-export default function Layout({title, children }) {
+export default function Layout({ title, children }) {
   return (
     <div>
       <>
         <Head>
-          <title>{title? title + ' - Lapage ':'Lapage'}</title>
+          <title>{title ? title + ' - Lapage ' : 'Lapage'}</title>
           <meta name="description" content="Ecommerce Website" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div className='flex justify-between min-h-screen flex-col'>
+        <div className="flex justify-between min-h-screen flex-col">
           <header>
             <NavBar />
           </header>
-          <main>{children}</main>
-          <footer>footer</footer>
+          <main className="container mt-4 px-4 m-auto">{children}</main>
+          <footer className='flex items-center justify-center h-10 shadow-inner'>
+            <Footer />
+          </footer>
         </div>
       </>
     </div>
