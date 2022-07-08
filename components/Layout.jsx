792a85629f081ac95a10e94f1b7172989d04f4d0
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-08 11:04:34 +0200

    Add navBar component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Layout({title, children }) {
@@ -10,8 +11,18 @@ export default function Layout({title, children }) {
           <meta name="description" content="Ecommerce Website" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div>
-          <header>headr</header>
+        <div className='flex justify-between min-h-screen flex-col'>
+          <header>
+            <nav className='flex shadow-md items-center px-4 justify-between h-12'>
+              <Link href="/">
+                <a className='font-bold text-lg'>Lapage</a>
+              </Link>
+              <div>
+                <Link href="/cart">Cart</Link>
+                <Link href="/login">Login</Link>
+              </div>
+            </nav>
+          </header>
           <main>{children}</main>
           <footer>footer</footer>
         </div>
