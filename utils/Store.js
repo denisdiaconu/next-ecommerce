776877d9730e6002fcdc5dda18444dc38b2867f7
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-11 10:43:57 +0200

    Add store to app

--- utils/Store.js
+++ utils/Store.js
@@ -20,5 +20,13 @@ function reducer(state, action) {
         : [...state.cart.cartItems, newItem];
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+    default:
+      return state;
   }
+}
+
+export function StoreProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{children}</Store.Provider>;
 }
