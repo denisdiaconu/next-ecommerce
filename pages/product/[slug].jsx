fa96452ef2453249f1bdad9ad368edf36e152f82
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-11 10:06:59 +0200

    Add style for product page

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 import Layout from '../../components/Layout';
@@ -6,11 +8,52 @@ import data from '../../utils/data';
 export default function ProductScreen() {
   const { query } = useRouter();
   const { slug } = query;
-  const product = data.products.find((x) => x.slug === slug)
+  const product = data.products.find((x) => x.slug === slug);
   if (!product) {
-    return <div>Product not found!</div>
+    return <div>Product not found!</div>;
   }
-  return <Layout title={product.name}>
-    <h1>{product.name}</h1>
-  </Layout>;
+  return (
+    <Layout title={product.name}>
+      <div className="py-2">
+        <Link href="/">back to products</Link>
+      </div>
+      <div className="grid md:gap-3 md:grid-cols-4">
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          />
+        </div>
+        <div>
+          <ul>
+            <li>
+              <h1 className="text-lg">{product.name}</h1>
+            </li>
+            <li>Category: {product.category}</li>
+            <li>Brand: {product.brand}</li>
+            <li>
+              {product.rating} of {product.numReviews} reviews
+            </li>
+            <li>Description: {product.description}</li>
+          </ul>
+        </div>
+        <div>
+          <div className='block rounded-lg border border-gray-200  shadow-md; p-5'>
+            <div className='mb-2 flex justify-between'>
+              <div>Price</div>
+              <div>${product.price}</div>
+            </div>
+            <div className='mb-2 flex justify-between'>
+              <div>Status</div>
+              <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
+            </div>
+            <button className='w-full rounded bg-amber-300 py-2 px-4 shadow outline-none hover:bg-amber-400  active:bg-amber-500'>Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
