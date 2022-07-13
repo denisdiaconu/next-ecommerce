44e7a7ef456a4f36b8a9e5efa0a747fcacaedc16
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-13 10:30:18 +0200

    Add login validation

--- pages/login.js
+++ pages/login.js
@@ -9,18 +9,49 @@ export default function LoginScreen() {
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const submitHandler = ({ email, password }) => {};
 
   return (
     <Layout title="Login">
-      <form className="mx-auto max-w-screen-md">
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
         <h1 className="text-xl mb-4">Login</h1>
         <div className="mb-4">
           <label htmlFor="email">Email</label>
-          <input type="email" className="w-full" id="email" autoFocus />
+          <input
+            type="email"
+            {...register('email', {
+              required: 'Please enter email',
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Please enter valid email',
+              },
+            })}
+            className="w-full"
+            id="email"
+            autoFocus
+          />
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="password">Password</label>
-          <input type="password" className="w-full" id="password" autoFocus />
+          <input
+            type="password"
+            {...register('password', {
+              required: 'Please enter password',
+              minLength: { value: 6, message: 'password is more than 5 chars' },
+            })}
+            className="w-full"
+            id="password"
+            autoFocus
+          />
+          {errors.password && (
+            <div className="text-red-500">{errors.password.message}</div>
+          )}
         </div>
         <div className="mb-4">
           <button className="rounded bg-amber-300 py-2 px-4 shadow outline-none hover:bg-amber-400 active:bg-amber-500">
