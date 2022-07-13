23cda1e0beea8808fadd062859bbe3e62fb64c8f
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-13 10:16:43 +0200

    Add react-hook-form

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Layout from '../components/Layout';
 
 export default function LoginScreen() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <Layout title="Login">
       <form className="mx-auto max-w-screen-md">
@@ -16,11 +23,13 @@ export default function LoginScreen() {
           <input type="password" className="w-full" id="password" autoFocus />
         </div>
         <div className="mb-4">
-          <button className="rounded bg-amber-300 py-2 px-4 shadow outline-none hover:bg-amber-400 active:bg-amber-500">Login</button>
+          <button className="rounded bg-amber-300 py-2 px-4 shadow outline-none hover:bg-amber-400 active:bg-amber-500">
+            Login
+          </button>
         </div>
         <div className="mb-4">
-            Don&apos;t have an account? &nbsp;
-            <Link href="register">Register</Link>
+          Don&apos;t have an account? &nbsp;
+          <Link href="register">Register</Link>
         </div>
       </form>
     </Layout>
