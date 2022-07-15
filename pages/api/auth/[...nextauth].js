40f8e2bb11a4bf82efeaf41cebbdacc8ca3c461c
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-15 12:22:47 +0200

    Add providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,8 @@
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import User from '../../../models/User';
+import db from '../../../utils/db';
+import bcryptjs from 'bcryptjs';
 
 export default NextAuth({
   session: {
@@ -16,4 +20,25 @@ export default NextAuth({
       return session;
     },
   },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        await db.connect();
+        const user = await User.findOne({
+          email: credentials.email,
+        });
+        await db.disconnect();
+        if (user && bcryptjs.compareSync(credentials.password, user.password)) {
+          return {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            image: 'f',
+            isAdmin: user.isAdmin,
+          };
+        }
+        throw new Error('Invalid email or password');
+      },
+    }),
+  ],
 });
