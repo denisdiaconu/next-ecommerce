bb8531f7ce30543810aafcf687bafc5367cace60
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-18 10:55:36 +0200

    Add shipping page

---
+++ pages/shipping.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function shipping() {
+  return (
+    <Layout title='Shipping Address'>
+    </Layout>
+  )
+}
