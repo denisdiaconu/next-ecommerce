4cba39a0835f1f295eb1129a0392ecf1b132ab18
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-19 10:55:32 +0200

    Add layout to payment page

--- pages/payment.js
+++ pages/payment.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useContext, useEffect, useState } from 'react';
 import CheckoutWizard from '../components/CheckoutWizard';
 import Layout from '../components/Layout';
+import { Store } from '../utils/Store';
 
 export default function PaymentScreen() {
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('');
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const { shippingAddress, paymentMethod } = cart;
+  const router = useRouter();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      return router.push('/shipping');
+    }
+    setSelectedPaymentMethod(paymentMethod || '');
+  }, [paymentMethod, router, shippingAddress.address]);
   return (
-    <Layout title='Payment Method'>
-        <CheckoutWizard activeStep={2} />
+    <Layout title="Payment Method">
+      <CheckoutWizard activeStep={2} />
+      <form onSubmit={submitHandler} className="max-w-screen-md mx-auto">
+        <h1 className="text-xl mb-4">Payment Method</h1>
+        {['PayPal', 'Stripe', 'CashOnDelivery'].map((payment) => (
+          <div key={payment} className="mb-4">
+            <input
+              onChange={() => setSelectedPaymentMethod(payment)}
+              type="radio"
+              name="paymentMethod"
+              className="outline-none p-2 focus:ring-0"
+              id={payment}
+              checked={selectedPaymentMethod === payment}
+            />
+            <label htmlFor={payment} className="p-2">
+              {payment}
+            </label>
+          </div>
+        ))}
+        <div className="flex justify-between mb-4">
+          <button
+            onClick={() => router.psuh('shipping')}
+            type="button"
+            className="rounded bg-gray-100 py-2  px-4 text-black shadow outline-none hover:bg-gray-200  active:bg-gray-300"
+          >
+            Back
+          </button>
+          <button className="rounded bg-amber-300 py-2 px-4 shadow outline-none hover:bg-amber-400 active:bg-amber-500">
+            Next
+          </button>
+        </div>
+      </form>
     </Layout>
-  )
+  );
 }
