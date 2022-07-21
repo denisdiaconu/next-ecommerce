5a664e65c872266542d35541fec30eaa614643d8
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-21 11:00:09 +0200

    Add fetch order function

---
+++ pages/order/[id].js
@@ -0,0 +1,45 @@
+import React, { useEffect, useReducer } from 'react';
+import Layout from '../../components/Layout';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { getError } from '../../utils/error';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, order: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
+
+function OrderScreen() {
+  const { query } = useRouter();
+  const orderId = query.id;
+
+  const [{ loading, error, order }, dispatch] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: '',
+  });
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/orders/${orderId}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchOrder();
+  }, [orderId]);
+  return <Layout title={`Order ${orderId}`}></Layout>;
+}
+
+OrderScreen.auth = true;
+export default OrderScreen;
