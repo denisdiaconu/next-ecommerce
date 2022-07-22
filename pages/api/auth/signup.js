b569b43f65cb5e9dad7653e20d2b2fa486cedddb
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-22 12:10:41 +0200

    Add signup validations

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,6 @@
 import User from '../../../models/User';
 import db from '../../../utils/db';
+import bcryptjs from 'bcryptjs';
 
 
 async function handler(req, res) {
@@ -26,6 +27,22 @@ async function handler(req, res) {
     await db.disconnect();
     return
   }
+  const newUser = new User({
+    name,
+    email,
+    password: bcryptjs.hashSync(password),
+    isAdmin: false,
+  })
+
+  const user = await newUser.save();
+  await db.disconnect();
+  res.status(201).send({
+    message: 'Created user',
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  })
 }
 
 export default handler;
